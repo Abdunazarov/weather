bc544e3ff20ee07f27fc3f37386a49437dedb4ac
Abdunazarov <abdunazarovdior@gmail.com>
2024-02-16 18:01:15 +0800

    Fixed __init__ & added README.md

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,25 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
-# Конфигурация приложения
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 # Инициализация расширения Flask-Migrate для управления миграциями базы данных
-migrate = Migrate(app, db)
+migrate = Migrate()
 
-def create_app(config_filename=None):
-    """
-    Фабрика приложений. Создает и настраивает экземпляр Flask приложения.
-    """
-    if config_filename:
-        app.config.from_pyfile(config_filename)
-    
+
+def create_app():
+    app = Flask(__name__)
+
+    # Конфигурация приложения
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../instance/sqlite.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from . import routes
+
+    app.register_blueprint(routes.bp)
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
-from flask import request, jsonify
 from http import HTTPStatus
-from . import app
-from .services import update_user_balance, get_user_by_id, create_user, delete_user
+
+from flask import Blueprint, jsonify, request
+
+from .services import create_user, delete_user, get_user_by_id, update_user_balance
 from .weather_service import fetch_weather
+
+bp = Blueprint("routes", __name__)
+
 
 def validate_balance(balance):
     """Проверка корректности баланса."""
@@ -10,10 +14,11 @@ def validate_balance(balance):
         return False
     return True
 
-@app.route('/create-user', methods=['POST'])
+
+@bp.route("/create-user", methods=["POST"])
 def create_user_route():
     data = request.get_json()
-    username, balance = data.get('username'), data.get('balance')
+    username, balance = data.get("username"), data.get("balance")
 
     if not username or balance is None:
         return jsonify({"error": "Missing username or balance"}), HTTPStatus.BAD_REQUEST
@@ -22,9 +27,20 @@ def create_user_route():
         return jsonify({"error": "The balance should be between 5000-15000"}), HTTPStatus.BAD_REQUEST
 
     user = create_user(username, balance)
-    return jsonify({"message": "User created successfully", "userId": user.id, "username": user.username, "balance": user.balance}), HTTPStatus.CREATED
+    return (
+        jsonify(
+            {
+                "message": "User created successfully",
+                "userId": user.id,
+                "username": user.username,
+                "balance": user.balance,
+            }
+        ),
+        HTTPStatus.CREATED,
+    )
 
-@app.route('/delete-user/<int:user_id>', methods=['DELETE'])
+
+@bp.route("/delete-user/<int:user_id>", methods=["DELETE"])
 def delete_user_route(user_id):
     if not get_user_by_id(user_id):
         return jsonify({"error": "User not found"}), HTTPStatus.NOT_FOUND
@@ -33,9 +49,10 @@ def delete_user_route(user_id):
         return jsonify({"message": "User deleted successfully"}), HTTPStatus.OK
     return jsonify({"error": "Failed to delete user"}), HTTPStatus.INTERNAL_SERVER_ERROR
 
-@app.route('/update-balance', methods=['POST'])
+
+@bp.route("/update-balance", methods=["POST"])
 def update_balance():
-    user_id, city = request.json.get('userId'), request.json.get('city')
+    user_id, city = request.json.get("userId"), request.json.get("city")
 
     if not user_id or not city:
         return jsonify({"error": "Missing userId or city"}), HTTPStatus.BAD_REQUEST
@@ -51,7 +68,16 @@ def update_balance():
 
         updated_user = update_user_balance(user_id, temperature)
         if updated_user:
-            return jsonify({"message": "Balance updated successfully", "userId": updated_user.id, "newBalance": updated_user.balance}), HTTPStatus.OK
+            return (
+                jsonify(
+                    {
+                        "message": "Balance updated successfully",
+                        "userId": updated_user.id,
+                        "newBalance": updated_user.balance,
+                    }
+                ),
+                HTTPStatus.OK,
+            )
     except Exception as e:
         print(e)
 
